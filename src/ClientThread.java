7148f119792008bd9e5d15cfd7058d7e3c0ae485
Joel Petersson <joel.anders.petersson@gmail.com>
2019-02-27 16:55:49 +0100

    Done

--- src/ClientThread.java
+++ src/ClientThread.java
@@ -5,7 +5,6 @@ public class ClientThread extends Thread {
 
     private Socket socket;
     private static final int FILE_SIZE = 10000;
-    private String FILE_TO_RECEIVE;
 
     public ClientThread(Socket socket){
        this.socket = socket;
@@ -13,22 +12,48 @@ public class ClientThread extends Thread {
     @Override
     public void run() {
         System.out.println("New connection now working.");
-        DataInputStream commandInput = new DataInputStream(socket.getInputStream());
-        String command = commandInput.readUTF();
-        if(command.equals("SAVE")) {
-            receiveSaveFile();
-        } else if(command.equals("LOAD")) {
-            
+        try {
+            DataInputStream commandInput = new DataInputStream(socket.getInputStream());
+            String command = commandInput.readUTF();
+            if (command.equals("SAVE")) {
+                receiveSaveFile();
+            } else if (command.equals("LOAD")) {
+                sendSaveFile();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
         }
 
 
     }
 
+    public void sendSaveFile() {
+        try {
+            DataInputStream receiveFileName = new DataInputStream(socket.getInputStream());
+            String FILE_TO_SEND = (String) receiveFileName.readUTF();
+            File savefile = new File(FILE_TO_SEND + ".txt");
+            byte[] fileSize = new byte[(int) savefile.length()];
+            FileInputStream fileIn = new FileInputStream(savefile);
+            BufferedInputStream fileBIn = new BufferedInputStream(fileIn);
+            fileBIn.read(fileSize, 0, fileSize.length);
+            OutputStream output = socket.getOutputStream();
+            System.out.println("Sending file(" + fileSize.length + " bytes)");
+            output.write(fileSize, 0, fileSize.length);
+            output.flush();
+            fileBIn.close();
+            fileIn.close();
+            System.out.println("File" + FILE_TO_SEND + ".txt sent");
+        } catch (Exception e) {
+
+        }
+    }
+
     public void receiveSaveFile() {
         try
         {
+
             DataInputStream receiveFileName = new DataInputStream(socket.getInputStream());
-            FILE_TO_RECEIVE = (String) receiveFileName.readUTF();
+            String FILE_TO_RECEIVE = (String) receiveFileName.readUTF();
 
             byte[] fileSize = new byte[FILE_SIZE];
             InputStream input = socket.getInputStream();
